05dd19c7edcf1031ec7da8e86d612b4230ac1e63
justinjacobsumaya8 <justinjacobsumaya8@gmail.com>
2023-07-09 21:53:47 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,125 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import Image from 'next/image';
 
-const inter = Inter({ subsets: ['latin'] })
+const PLUS_JAKARTA_SANS = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+    return (
+        <main className={`${PLUS_JAKARTA_SANS.className}`}>
+            <section id="landing-section">
+                <div className="relative lg:bg-[url('/images/main-bg.png')] bg-center bg-cover h-screen">
+                    <div className="p-5 bg-white absolute w-full z-10 opacity-25"></div>
+                    <div className="absolute bg-dark-green text-white h-screen w-full lg:w-[44%]">
+                        <div className="text-center lg:text-left px-10 lg:px-16 py-8 mt-8">
+                            <h3 className="text-2xl">Brand New Company</h3>
+                        </div>
+                        <div className="flex align-middle items-center justify-center h-[70%] lg:h-[80%]">
+                            <div className="flex items-stretch justify-between mt-5">
+                                <div>
+                                    <div className="px-12 lg:pt-20 lg:p-32">
+                                        <div>
+                                            <h1 className="text-[35px] leading-[38.5px] lg:text-5xl">Launching 2023</h1>
+                                            <p className="mt-9 text-[14px] leading-[22.4px] lg:text-xl pr-5 lg:pr-0">We are in the middle of updating our website but are still very much available to help you with any upcoming projects.</p>
+                                            <p className="mt-6 font-extralight text-[14px] leading-[22.4px]">Contact us or come and visit our office.</p>
+                                        </div>
+                                        <div className="my-9 ml-1 lg:ml-0">
+                                            <a href="#" className="bg-visit-office-link text-black text-xs rounded-full px-12 py-3">
+                                                Visit office
+                                            </a>
+                                        </div>
+                                        <div className="lg:mt-20">
+                                            <div>
+                                                <span className="text-xs">EMAIL:&nbsp;&nbsp; <span className="font-extralight">test@rigo.agency</span></span>
+                                            </div>
+                                            <div className="mt-3">
+                                                <span className="text-xs">PHONE:&nbsp;&nbsp; <span className="font-extralight">02071231234</span></span>
+                                            </div>
+                                            <div className="mt-3">
+                                                <span className="text-xs">ADDRESS:&nbsp;&nbsp; <span className="font-extralight">Big Ben, London, SWA1A OAA</span></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <Image
+                                        src="/images/main-bg-cropped.png"
+                                        alt="Main BG"
+                                        width={0}
+                                        height={0}
+                                        sizes="100vh"
+                                        className="lg:hidden w-72 h-full"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="subscribe-section">
+                <div className="py-32 px-10 lg:p-40 lg:pb-32 text-center">
+                    <h6 className="text-semibold uppercase">Keep up to date - Subscribe to our Newsletter</h6>
+                    <p className="mt-5 font-extralight text-md">Receive the latest offers, promotions and the news delivered <br className="hidden lg:block" />straight to your inbox with our exclusive email newsletter.</p>
+                    <input type="text" className="border border-black w-full lg:w-[30%] px-4 py-2 text-[10px] mt-10" placeholder="Email Address" />
+                </div>
+            </section>
+            <section id="instagram-section">
+                <div className="pb-14">
+                    <h3 className="text-xl lg:text-2xl text-center lg:text-left mx-16 my-8">Check out our latest projects on instagram</h3>
+                    <div className="overflow-auto whitespace-nowrap flex gap-5">
+                        <Image src="/images/instagram/image-4.png" alt="Instagram" width={300} height={300} className="cursor-pointer w-full h-auto" />
+                        <Image src="/images/instagram/image-1.png" alt="Instagram" width={300} height={300} className="cursor-pointer w-full h-auto" />
+                        <Image src="/images/instagram/image-2.png" alt="Instagram" width={300} height={300} className="cursor-pointer w-full h-auto" />
+                        <Image src="/images/instagram/image-3.png" alt="Instagram" width={300} height={300} className="cursor-pointer w-full h-auto" />
+                        <Image src="/images/instagram/image-4.png" alt="Instagram" width={300} height={300} className="cursor-pointer w-full h-auto" />
+                        <Image src="/images/instagram/image-1.png" alt="Instagram" width={400} height={300} className="cursor-pointer w-full h-auto" />
+                    </div>
+                </div>
+            </section>
+            <section id="contact-section">
+                <div className="bg-contact-section p-12 lg:p-16">
+                    <div className="flex flex-col lg:flex-row justify-between">
+                        <div className="flex flex-col lg:flex-row text-center lg:text-left gap-16">
+                            <div>
+                                <div>
+                                    <div className="uppercase font-medium text-[10px] leading-[12px] lg:text-xs">Address</div>
+                                    <div className="mt-3 font-extralight text-[10px] leading-[12px] lg:text-xs">
+                                        <div>Big Ben,</div>
+                                        <div className="mt-2">London, SW1A OAA</div>
+                                    </div>
+                                </div>
+                                <div className="mt-10">
+                                    <div className="uppercase font-medium text-[10px] leading-[12px] lg:text-xs">Opening Times:</div>
+                                    <div className="font-extralight mt-3 text-[10px] leading-[12px] lg:text-xs">
+                                        <div className="mt-2">Mon - Fri: 8am-5:30pm</div>
+                                        <div className="mt-2">Saturday: 10am-5pm</div>
+                                        <div className="mt-2">Sunday: Closed</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="uppercase font-medium text-[10px] leading-[12px] lg:text-xs">Call Us</div>
+                                <div className="mt-3 font-extralight text-[10px] leading-[12px] lg:text-xs">02071231234</div>
+                            </div>
+                        </div>
+                        <div className="flex justify-center gap-6 mt-10 lg:mt-0">
+                            <a href="#!">
+                                <Image src="/icons/instagram.svg" alt="Instagram Icon" width={20} height={20} />
+                            </a>
+                            <a href="#!">
+                                <Image src="/icons/facebook.svg" alt="Facebook Icon" width={20} height={20} />
+                            </a>
+                            <a href="#!">
+                                <Image src="/icons/linkedin.svg" alt="LinkedIn Icon" width={20} height={20} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="section-copyright">
+                <div className="bg-dark-green text-white text-center px-10 py-4">
+                    <span className="uppercase text-[8px] lg:text-[10px] leading-[12.8px] lg:lleading-4 font-extralight tracking-widest">Copyright ©2023 BRANDNEWCOMPANY.com. All Rights Reserved</span>
+                </div>
+            </section>
+        </main>
+    );
+};
